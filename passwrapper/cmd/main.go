553beaf11c6f295adaf079ac975648b34e94eb43
alexpfx <alexpfba@gmail.com>
2022-02-15 19:17:08 -0300

    docker

--- passwrapper/cmd/main.go
+++ passwrapper/cmd/main.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/alexpfx/go_sh/common/util"
-	"github.com/urfave/cli/v2"
-	"io/ioutil"
 	"log"
 	"os"
 	"passwrapper"
 
-	"path/filepath"
-	"time"
+	"github.com/urfave/cli/v2"
 )
 
 var letterCharset string
 var numberCharset string
 var specialCharset string
-var configArg string
 var length int
 var specialCount int
 var numberCount int
@@ -25,51 +19,32 @@ var upperCaseCount int
 var lowerCaseCount int
 
 func main() {
-	configDir, err := os.UserConfigDir()
-	defaultConfigPath := filepath.Join(configDir, "go_sh/passwrapper/config.json")
-
-	var update bool
-
+	
 	app := &cli.App{
 		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:        "config",
-				Aliases:     []string{"c"},
-				Usage:       "caminho do arquivo de configuração",
-				FilePath:    defaultConfigPath,
-				TakesFile:   true,
-				Destination: &configArg,
-			},
-			&cli.BoolFlag{
-				Name:        "update",
-				Aliases:     []string{"u"},
-				Usage:       "gera um novo arquivo de configuração e termina a execução",
-				Value:       false,
-				Destination: &update,
-			},
 			&cli.IntFlag{
 				Name:        "length",
 				Aliases:     []string{"s"},
 				Usage:       "tamanho da senha",
-				Value:       12,
+				Value:       14,
 				Destination: &length,
 			},
 			&cli.IntFlag{
 				Name:        "uppercase",
 				Usage:       "quantidade mínima de letras maíusculas",
-				Value:       1,
+				Value:       2,
 				Destination: &upperCaseCount,
 			},
 			&cli.IntFlag{
 				Name:        "lowercase",
 				Usage:       "quantidade mínima de letras minúsculas",
-				Value:       1,
+				Value:       2,
 				Destination: &lowerCaseCount,
 			},
 			&cli.IntFlag{
 				Name:        "numbers",
 				Usage:       "quantidade mínima de números",
-				Value:       1,
+				Value:       2,
 				Destination: &numberCount,
 			},
 			&cli.IntFlag{
@@ -96,18 +71,7 @@ func main() {
 			},
 		},
 		Action: func(c *cli.Context) error {
-			if update {
-				log.Println("updating")
-				updateConfig(defaultConfigPath)
-				return nil
-			}
-
-
-			var config passwrapper.Config
-
-			err = json.Unmarshal([]byte(configArg), &config)
-			util.CheckFatal(err, "Não pode carregar arquivo de configuração")
-
+			
 			pass := passwrapper.Pass{
 				Config: passwrapper.Config{
 					LetterCharset:  letterCharset,
@@ -126,27 +90,8 @@ func main() {
 		},
 	}
 
-	err = app.Run(os.Args)
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func updateConfig(configPath string) {
-	newConfig := passwrapper.Config{
-		LetterCharset:  letterCharset,
-		NumberCharset:  numberCharset,
-		SpecialCharset: specialCharset,
-	}
-
-	prettyJson, err := json.MarshalIndent(newConfig, "", "\t")
-	util.CheckFatal(err, "")
-
-	if util.FileExists(configPath) {
-		backFile := fmt.Sprintf("%s_%s.bkp", configPath, time.Now().Format("20060102_150405.000000"))
-		util.MoveFile(configPath, backFile)
-	}
-	err = ioutil.WriteFile(configPath, prettyJson, 0644)
-	util.CheckFatal(err, "não pode escrever em arquivo de configuração")
-
 }
